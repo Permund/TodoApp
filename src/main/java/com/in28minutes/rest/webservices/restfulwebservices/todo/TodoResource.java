dd57c5d4264e5771746d1b0be7bae5d13f8a504b
Per Gravdal <per.gravdal@hotmail.com>
2020-11-12 14:05:54 +0100

    allow heroku origin

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -11,7 +11,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200", "https://todo-scano-react.herokuapp.com"})
 public class TodoResource {
 
     @Autowired
