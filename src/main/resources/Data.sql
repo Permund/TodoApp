eb765726e39df637b494ca9b0d197e784ae65c86
Per Gravdal <per.gravdal@hotmail.com>
2020-11-12 08:42:09 +0100

    insert data

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -1,8 +1,8 @@
 insert into todo(id, username, description, target_date, is_done)
-values(10001, 'scano', 'Learn JPA', sysdate(), false);
+values(10001, 'scano', 'Activity 1', sysdate(), false);
 
 insert into todo(id, username, description, target_date, is_done)
-values(10002, 'scano', 'Learn Spring', sysdate(), false);
+values(10002, 'scano', 'Activity 2', sysdate(), false);
 
 insert into todo(id, username, description, target_date, is_done)
-values(10003, 'scano', 'Learn to play', sysdate(), false);
+values(10003, 'scano', 'Activity 3', sysdate(), false);
